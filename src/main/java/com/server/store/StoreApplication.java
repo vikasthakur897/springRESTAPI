3ebf3e38db3b41d4b35bc7a05e6eb674354098ab
VikasThakur <vikasthakur1723@gmail.com>
2025-10-19 19:07:49 +0530

    Add enviroment variable

--- src/main/java/com/server/store/StoreApplication.java
+++ src/main/java/com/server/store/StoreApplication.java
@@ -3,11 +3,19 @@ package com.server.store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class StoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StoreApplication.class, args);
-	}
+	  public static void main(String[] args) {
+        // Load .env variables before Spring starts
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry ->
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
+
+        SpringApplication.run(StoreApplication.class, args);
+    }
 
 }
